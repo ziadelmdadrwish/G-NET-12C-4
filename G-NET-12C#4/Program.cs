b09a99a532d37b4eb323cca4b72b8bea35e35a72
IT <IT@MrVirus-2026AEM>
2026-02-19 14:34:10 +0200

    Add project files.

---
+++ G-NET-12C#4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_NET_12C_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
